07e3d9d6ac28035fc0817ea23e7676f3daefb70b
Michael Ashton <mykl.ashton@gmail.com>
2017-07-01 13:21:49 -0400

    Commit after creating models

---
+++ models/user.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define('user', {
+    name: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return user;
+};
