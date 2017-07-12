39ddd0bbe4b7a6162aad6c61b5081b240909971d
Michael Ashton <mykl.ashton@gmail.com>
2017-07-12 12:46:07 -0400

    hurrumph

--- app.js
+++ app.js
@@ -78,6 +78,10 @@ app.use(morgan('dev'));
 app.use(routes);
 
 
-app.listen(8080, function(req, res){
-  console.log("Server running, listening on port 8080. CRUSH IT!!!");
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+// app.listen(8080, function(req, res){
+//   console.log("Server running, listening on port 8080. CRUSH IT!!!");
+// })
